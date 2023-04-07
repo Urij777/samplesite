48d3d25b7d1b80954f82b29485f17711e24dfe27
Urij <urijlamihin@gmail.com>
2023-04-07 15:25:51 +1000

    django_3.0/3

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .forms import BbForm
 from .models import Bb, Rubric
@@ -7,7 +8,7 @@ from .models import Bb, Rubric
 class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
-    success_url = '/bboard'
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
